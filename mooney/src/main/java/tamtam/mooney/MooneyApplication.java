e5de34821ee5671e681090a5550f1831c9f5744c
Seohyun Lee <applemint2003@gmail.com>
2025-02-27 16:27:15 +0900

    feat: 프로젝트 생성

---
+++ mooney/src/main/java/tamtam/mooney/MooneyApplication.java
@@ -0,0 +1,13 @@
+package tamtam.mooney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MooneyApplication.class, args);
+    }
+
+}
---
+++ mooney/src/test/java/tamtam/mooney/MooneyApplicationTests.java
@@ -0,0 +1,13 @@
+package tamtam.mooney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
