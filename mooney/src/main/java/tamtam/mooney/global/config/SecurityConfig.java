80e00a46577f8a9f9d85cb1359a9a42372ef6523
Seohyun Lee <applemint2003@gmail.com>
2025-02-28 16:10:46 +0900

    fix: Spring Security의 hasAnyRole 사용으로 인한 에러 수정

--- mooney/src/main/java/tamtam/mooney/global/config/SecurityConfig.java
+++ mooney/src/main/java/tamtam/mooney/global/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
                                 "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs", "/v3/api-docs/**").permitAll()
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .requestMatchers("/auth/**", "/public/**").permitAll()
-                        .requestMatchers("/admin/**").hasRole(String.valueOf(Role.ROLE_ADMIN))
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().permitAll()); // TODO: 추후 수정
         ;
         return http.build();
