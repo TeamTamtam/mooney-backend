5d3b3bc80970aafc774330fba16c860605bf65c3
Seohyun Lee <applemint2003@gmail.com>
2025-03-03 15:02:07 +0900

    feat: 월별 예산 페이지 API 개발 중

---
+++ mooney/src/main/java/tamtam/mooney/domain/budget/dto/MonthlyBudgetProgressResponseDto.java
@@ -0,0 +1,15 @@
+package tamtam.mooney.domain.budget.dto;
+
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record MonthlyBudgetProgressResponseDto(
+        Long remainingBudget,
+        Long dailyBudget,
+        Long monthlyBudget,
+        Long recurringExpense,
+        Long totalExpense,
+        List<CategoryBudgetDto> categoryBudgets
+) {}
