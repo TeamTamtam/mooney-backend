ecdf90102b3b4140308d2b7cc3795fd22c952cf9
Seohyun Lee <applemint2003@gmail.com>
2025-09-07 17:01:29 +0900

    refactor: ChatController에 API 설명 추가

--- mooney/src/main/java/tamtam/mooney/domain/chat/controller/ChatController.java
+++ mooney/src/main/java/tamtam/mooney/domain/chat/controller/ChatController.java
@@ -1,5 +1,6 @@
 package tamtam.mooney.domain.chat.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ import tamtam.mooney.domain.chat.service.ChatService;
 public class ChatController {
     private final ChatService chatService;
 
+    @Operation(summary = "똑똑소비봇에게 채팅 전송", description = "똑똑소비봇에게 채팅 메시지를 전송합니다.")
     @PostMapping
     public ResponseEntity<ChatResponseDto> chat(@RequestBody @Valid ChatRequestDto requestDto) {
         return ResponseEntity.ok().body(chatService.chat(requestDto));
